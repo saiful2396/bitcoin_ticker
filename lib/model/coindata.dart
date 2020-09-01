ba0f40613efe6daeb1c5e4becd46266aa7f209e3
Saiful Islam <saifulislampp23@gmail.com>
2020-09-02 01:58:40 +0600

    json data fetch

--- lib/model/coindata.dart
+++ lib/model/coindata.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 const List<String> currenciesList = [
   'AUD',
-  'BDT'
+  'BDT',
   'BRL',
   'CAD',
   'CNY',
@@ -27,7 +30,27 @@ const List<String> cryptoList = [
   'ETH',
   'LTC',
 ];
+const apiKey = '0BFBADF8-81E0-4A31-881E-9C33068F9AA0';
+const url = 'https://rest.coinapi.io/v1/exchangerate';
 
-class CoinData{
-  //to do something;
+class CoinData {
+  final String asset_id_base;
+  final String asset_id_quote;
+  final int rate;
+
+  CoinData({this.asset_id_base, this.asset_id_quote, this.rate});
+
+  Future getCoinData() async {
+    String requestURL = '$url/BTC/USD?apikey=$apiKey';
+    var response = await http.get(requestURL);
+
+    if(response.statusCode == 200){
+      var decodeData = jsonDecode(response.body);
+      var rate = decodeData['rate'];
+    return rate;
+    }else{
+      print(response.statusCode);
+    }
+  }
+
 }
--- lib/screen/bitcoin_ticker.dart
+++ lib/screen/bitcoin_ticker.dart
@@ -1,5 +1,8 @@
+import 'dart:io' show Platform;
+import 'package:bitcoin_ticker/model/coindata.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
 
 class BitcoinScreen extends StatefulWidget {
   @override
@@ -9,6 +12,62 @@ class BitcoinScreen extends StatefulWidget {
 class _BitcoinScreenState extends State<BitcoinScreen> {
 
   String selectedCurrency = 'BDT';
+  String bitCoinValueInUSD = '?';
+
+  void getRate() async{
+    try {
+      double data = await CoinData().getCoinData();
+      setState(() {
+        bitCoinValueInUSD = data.toStringAsFixed(0);
+      });
+    }catch(e) {
+      print(e);
+    }
+  }
+  DropdownButton<String> androidDropdown() {
+    List<DropdownMenuItem<String>> dropDownItems = [];
+    for (String currency in currenciesList) {
+      var value = DropdownMenuItem(
+        child: Text(currency),
+        value: currency,
+      );
+      dropDownItems.add(value);
+    }
+    return DropdownButton<String>(
+      value: selectedCurrency,
+      items: dropDownItems,
+      onChanged: (value) {
+        setState(() {
+          selectedCurrency = value;
+        });
+      },
+    );
+  }
+
+  CupertinoPicker iOSPicker() {
+    List<Text> pickerItems = [];
+    for (String currency in currenciesList) {
+      pickerItems.add(Text(
+        currency,
+        style: TextStyle(color: Colors.white),
+      ));
+    }
+    return CupertinoPicker(
+      itemExtent: 32.0,
+      onSelectedItemChanged: (value) {
+        setState(() {
+          selectedCurrency = value.toString();
+        });
+      },
+      children: pickerItems,
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getRate();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,62 +76,27 @@ class _BitcoinScreenState extends State<BitcoinScreen> {
         title: Text('BitCoin Ticker App'),
         centerTitle: true,
       ),
-
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.teal[300],
+          Padding( padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+            child: Card( color: Colors.teal[300],
               elevation: 5,
               shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0)
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 15.0),
-                child: Text(
-                    '1 BTC = ? USD',
-                  textAlign: TextAlign.center,
+                  borderRadius: BorderRadius.circular( 10.0 ) ),
+              child: Padding( padding: EdgeInsets.symmetric(
+                  horizontal: 24.0, vertical: 15.0 ),
+                child: Text( '1 BTC = $bitCoinValueInUSD USD', textAlign: TextAlign.center,
                   style: TextStyle(
-                    fontSize: 20.0, color: Colors.white
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Container(
-            height: 150.0,
+                      fontSize: 20.0, color: Colors.white ), ), ), ), ),
+          Container( height: 150.0,
             alignment: Alignment.center,
-            padding: EdgeInsets.only(bottom: 30.0),
-            color: Theme.of(context).primaryColor,
-            child: DropdownButton(
-              value: '$selectedCurrency',
-              items: [
-                DropdownMenuItem(
-                  child: Text('BDT'),
-                  value: 'BDT',
-                ),
-                DropdownMenuItem(
-                  child: Text('USD'),
-                  value: 'USD',
-                ),
-                DropdownMenuItem(
-                  child: Text('EUR'),
-                  value: 'EUR',
-                ),
-              ],
-              onChanged: (value){
-                setState(() {
-                  selectedCurrency = value;
-                });
-              },
-            ),
-          ),
-        ],
-      ),
-
-    );
+            padding: EdgeInsets.only( bottom: 30.0 ),
+            color: Theme
+                .of( context )
+                .primaryColor,
+            child: Platform.isIOS ? iOSPicker( ) : androidDropdown( ), ),
+        ], ), );
   }
 }
